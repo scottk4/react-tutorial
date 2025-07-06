431ffc0c50eeaad667ee803b33931510b60e1de7
Scott Kelly <scott@Scotts-MacBook-Pro.local>
2025-07-06 11:42:41 -0600

    use Effect Hook resizing window

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,29 +1,34 @@
 import React, {useEffect, useState} from 'react'
 
 function MyComponent(){
+    
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
 
-    const [count, setCount] = useState(0);
-    const [color, setColor] = useState("green");
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        console.log("EVENT LISTENER ADDED");
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            console.log("EVENT LISTENER REMOVED");
+        }
+    }, []);
     
     useEffect(() => {
+        document.title = `Size: ${width} x ${height}`;
+    }, [width, height]);
 
-        document.title = `Count: ${count} ${color}`;
-    }, [count, color]);
-    function addCount(){
-        setCount(c => c + 1);
+    function handleResize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
     }
-    function subCount(){
-        setCount(c => c - 1);
-    }
-    function changeColor(){
-        setColor(c => c === "green" ? "red" : "green");
-    }
+
     return(<>
     
-        <p style={{color: color}}>Count: {count}</p>
-        <button onClick={addCount}>Add</button>
-        <button onClick={subCount}>Sub</button>
-        <button onClick={changeColor}>Change Color</button>
+        <p>Window Width: {width}px</p>
+        <p>Window Height: {height}px</p>
     </>);
 }
+
 export default MyComponent
